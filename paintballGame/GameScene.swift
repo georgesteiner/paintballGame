2e791526656746acbf6c2b83182a86f7962101f2
georgesteiner <georgeowensteiner@gmail.com>
2016-03-15 01:26:58 -0400

    computer player moving side to side and firing paintballs but glitches for some reason

--- paintballGame/GameScene.swift
+++ paintballGame/GameScene.swift
@@ -53,18 +53,14 @@ extension CGPoint {
 
 class GameScene: SKScene, SKPhysicsContactDelegate {
     
-    
-    
-    
-//    let roundPlayer = SKShapeNode(circleOfRadius: 10)
-    let roundPlayer = SKSpriteNode(imageNamed: "roundplayer20")
-    
-//    let roundPlayer = SKSpriteNode(color: UIColor.blackColor(), size: CGSize(width: 20, height: 20))
-    
+    let humanPlayer = SKSpriteNode(imageNamed: "roundplayer20")
+    let computerPlayer = SKSpriteNode(imageNamed: "roundplayer20")
     var leftButton: SKSpriteNode! = nil
     var rightButton: SKSpriteNode! = nil
     var upButton: SKSpriteNode! = nil
     var downButton: SKSpriteNode! = nil
+    var shotCounter: Int = 0
+    var shotCountLabel: SKLabelNode! = nil
     
     func setUpButtons() {
         leftButton = SKSpriteNode(imageNamed: "leftarrow64")
@@ -87,74 +83,54 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         downButton.position = CGPoint(x: leftButton.frame.width * 3, y: 0)
         self.addChild(downButton)
         
+        
+        shotCountLabel = SKLabelNode(text: "\(shotCounter)")
+        shotCountLabel.position = CGPoint(x: size.width * 0.85, y: size.height * 0.03)
+        self.addChild(shotCountLabel)
+        
     }
 
-    
-    
-    
-    
-    //bunkers
-    
-    
     func setUpBunkers() {
        
         
         let bunkerColor = UIColor(hue:1, saturation:1, brightness:0.69, alpha:1)
         
-        //bottom of field
+        var rows = 0
         
         
-        
-        let roundBunker = SKShapeNode(circleOfRadius: 20)
-        roundBunker.fillColor = bunkerColor
-        roundBunker.lineWidth = 0
-        roundBunker.position = CGPoint(x: size.width * 0.1, y: size.height * 0.2)
-        addChild(roundBunker)
+        let arrayOfWidth : [CGFloat]  = [0.1, 0.5, 0.9]
 
-        let roundBunker2 = SKShapeNode(circleOfRadius: 20)
-        roundBunker2.fillColor = bunkerColor
-        roundBunker2.lineWidth = 0
-        roundBunker2.position = CGPoint(x: size.width * 0.9, y: size.height * 0.2)
-        addChild(roundBunker2)
+        let arrayOfHeight : [CGFloat] = [0.2, 0.45, 0.65, 0.9]
+        
+        while rows < 4 {
+            print("rows \(rows)")
 
-        let squareBunker = SKSpriteNode(color: bunkerColor, size: CGSize(width: 40, height: 40))
-        squareBunker.position = CGPoint(x: size.width * 0.5, y: size.height * 0.2)
-        addChild(squareBunker)
-        
-        
-        
-//        let rectangleBunker = SKSpriteNode(color: bunkerColor, size: CGSize(width: 15, height: 10))
-        
-        var i = 0
-        
-        while i < 3 {
-            print("hello \(i)")
-            i++
+            var i = 0
+
+            while i < 3 {
+                print("i \(i)")
+                
+              
+                
+                let squareBunker = SKSpriteNode(color: bunkerColor, size: CGSize(width: 40, height: 40))
+                
+                squareBunker.position = CGPoint(x: size.width * arrayOfWidth[i], y: size.height * arrayOfHeight[rows])
+                
+                addChild(squareBunker)
+                
+                squareBunker.physicsBody = SKPhysicsBody(rectangleOfSize: squareBunker.size) // 1
+                squareBunker.physicsBody?.dynamic = false // 2
+                squareBunker.physicsBody?.categoryBitMask = PhysicsCategory.Bunker // 3
+                squareBunker.physicsBody?.contactTestBitMask = PhysicsCategory.Paintball // 4
+                squareBunker.physicsBody?.collisionBitMask = PhysicsCategory.Player // 5
+                
+                i++
+            }
             
-            let arrayOfWidth: Array[CGFloat] = [0.3, 0.5, 0.7]
-            let one = arrayOfWidth[0]
-            
-//            let xCoordinate: Double = size.width * 0.2 * i
-            
-            
-            let squareBunker = SKSpriteNode(color: bunkerColor, size: CGSize(width: 40, height: 40))
-            squareBunker.position = CGPoint(x: size.width * 0.3, y: size.height * 0.4)
-            addChild(squareBunker)
-            
-            squareBunker.physicsBody = SKPhysicsBody(rectangleOfSize: squareBunker.size) // 1
-            squareBunker.physicsBody?.dynamic = false // 2
-            squareBunker.physicsBody?.categoryBitMask = PhysicsCategory.Bunker // 3
-            squareBunker.physicsBody?.contactTestBitMask = PhysicsCategory.Paintball // 4
-            squareBunker.physicsBody?.collisionBitMask = PhysicsCategory.Player // 5
+            rows++
             
         }
 
-        squareBunker.physicsBody = SKPhysicsBody(rectangleOfSize: squareBunker.size) // 1
-        squareBunker.physicsBody?.dynamic = false // 2
-        squareBunker.physicsBody?.categoryBitMask = PhysicsCategory.Bunker // 3
-        squareBunker.physicsBody?.contactTestBitMask = PhysicsCategory.Paintball // 4
-        squareBunker.physicsBody?.collisionBitMask = PhysicsCategory.Player // 5
-        
         
     }
     
@@ -164,8 +140,8 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
 
         backgroundColor = SKColor(hue: 0.33, saturation: 1, brightness: 0.54, alpha: 1)
 
-        roundPlayer.position = CGPoint(x: size.width * 0.5, y: size.height * 0.15)
-        addChild(roundPlayer)
+        humanPlayer.position = CGPoint(x: size.width * 0.5, y: size.height * 0.15)
+        addChild(humanPlayer)
         
         setUpBunkers()
         setUpButtons()
@@ -173,19 +149,50 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         physicsWorld.gravity = CGVectorMake(0, 0)
         physicsWorld.contactDelegate = self
         
-        roundPlayer.physicsBody = SKPhysicsBody(circleOfRadius: roundPlayer.size.width / 2)
-        roundPlayer.physicsBody?.dynamic = true
-        roundPlayer.physicsBody?.categoryBitMask = PhysicsCategory.Player
-        roundPlayer.physicsBody?.collisionBitMask = PhysicsCategory.Bunker
+        humanPlayer.physicsBody = SKPhysicsBody(circleOfRadius: humanPlayer.size.width / 2)
+        humanPlayer.physicsBody?.dynamic = true
+        humanPlayer.physicsBody?.categoryBitMask = PhysicsCategory.Player
+        humanPlayer.physicsBody?.collisionBitMask = PhysicsCategory.Bunker
         
-//        roundPlayer.physicsBody?.categoryBitMask =
         
-//        let borderBody = SKPhysicsBody(edgeLoopFromRect: self.frame)
-//        borderBody.friction = 0
-//        self.physicsBody = borderBody
+        setUpOpponent()
         
         
         
+    }
+    
+    func setUpOpponent() {
+        computerPlayer.position = CGPoint(x: size.width * 0.5, y: size.height * 0.8)
+        addChild(computerPlayer)
+        
+        computerPlayer.physicsBody = SKPhysicsBody(circleOfRadius: computerPlayer.size.width / 2)
+        computerPlayer.physicsBody?.dynamic = true
+        computerPlayer.physicsBody?.categoryBitMask = PhysicsCategory.Player
+        computerPlayer.physicsBody?.contactTestBitMask = PhysicsCategory.Paintball
+        
+        
+        
+        let movePlayerRight = SKAction.moveToX(computerPlayer.position.x + 150, duration: 2)
+//        let movePlayerLeft = SKAction.moveToX(computerPlayer.position.x - 150, duration: 2)
+        let movePlayerLeft = SKAction.moveByX(-150, y: 0, duration: 2)
+        
+        let sequence = SKAction.sequence([movePlayerRight, movePlayerLeft])
+        
+        let sequenceRepeating = SKAction.repeatActionForever(sequence)
+        
+        computerPlayer.runAction(sequenceRepeating)
+        
+        let shotTime = NSTimer.scheduledTimerWithTimeInterval(2, target: self, selector: "shootPaintballForComputer", userInfo: nil, repeats: true)
+        
+        
+        shotTime.fire()
+        
+        
+        
+    }
+    
+    func shootPaintballForComputer() {
+        shootPaintballFromStartPositionTowards(computerPlayer.position, towardsPosition: humanPlayer.position)
     }
     
     
@@ -208,29 +215,29 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         if leftButton.containsPoint(touchLocation) {
             print("move left")
             
-            let movePlayerLeft = SKAction.moveToX(roundPlayer.position.x - 450, duration: 6)
-            roundPlayer.runAction(movePlayerLeft)
+            let movePlayerLeft = SKAction.moveToX(humanPlayer.position.x - 450, duration: 6)
+            humanPlayer.runAction(movePlayerLeft)
         }
         
         else if rightButton.containsPoint(touchLocation) {
             print("move right")
             
-            let movePlayerRight = SKAction.moveToX(roundPlayer.position.x + 450, duration: 6)
-            roundPlayer.runAction(movePlayerRight)
+            let movePlayerRight = SKAction.moveToX(humanPlayer.position.x + 450, duration: 6)
+            humanPlayer.runAction(movePlayerRight)
         }
         
         else if upButton.containsPoint(touchLocation) {
             print("move up")
             
-            let movePlayerUp = SKAction.moveToY(roundPlayer.position.y + 900, duration: 12)
-            roundPlayer.runAction(movePlayerUp)
+            let movePlayerUp = SKAction.moveToY(humanPlayer.position.y + 900, duration: 12)
+            humanPlayer.runAction(movePlayerUp)
         }
         
         else if downButton.containsPoint(touchLocation) {
             print("move down")
             
-            let movePlayerDown = SKAction.moveToY(roundPlayer.position.y - 900, duration: 12)
-            roundPlayer.runAction(movePlayerDown)
+            let movePlayerDown = SKAction.moveToY(humanPlayer.position.y - 900, duration: 12)
+            humanPlayer.runAction(movePlayerDown)
         }
         
         
@@ -238,6 +245,88 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
 
     }
     
+    
+    func shootPaintballFromStartPositionTowards(startPosition: CGPoint, towardsPosition: CGPoint){
+        
+        // 2 - Set up initial location of projectile
+        
+        let paintballShape = SKShapeNode(circleOfRadius: 3)
+        paintballShape.fillColor = UIColor.yellowColor()
+        paintballShape.lineWidth = 0
+        
+        let texture = view?.textureFromNode(paintballShape)
+        let paintball = SKSpriteNode(texture: texture)
+        
+        
+        
+        
+        
+        
+        let offset = towardsPosition - startPosition
+
+        let direction = offset.normalized()
+        print("direction \(direction)")
+        
+        var largerNumber: CGFloat
+        
+        if fabs(direction.x) > fabs(direction.y) {
+            largerNumber = direction.x
+        }
+        
+        else {
+            largerNumber = direction.y
+        }
+        
+        let multiplier = (((humanPlayer.size.width / 2) + (paintball.size.width / 2)) / largerNumber)
+        
+        paintball.position = CGPoint(x: startPosition.x + (direction.x * multiplier * 1.1), y: startPosition.y + (direction.y * multiplier * 1.1))
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        paintball.physicsBody = SKPhysicsBody(circleOfRadius: paintball.frame.width/2)
+        //check on this frame later
+        
+        paintball.physicsBody?.dynamic = true
+        paintball.physicsBody?.categoryBitMask = PhysicsCategory.Paintball
+        paintball.physicsBody?.contactTestBitMask = PhysicsCategory.Bunker
+        paintball.physicsBody?.collisionBitMask = PhysicsCategory.None
+        paintball.physicsBody?.usesPreciseCollisionDetection = true
+        
+        // 3 - Determine offset of location to projectile
+        
+        // 4 - Bail out if you are shooting down or backwards
+        //        if (offset.x < 0) { return }
+        
+        // 5 - OK to add now - you've double checked position
+        addChild(paintball)
+        
+        // 6 - Get the direction of where to shoot
+        
+        
+        // 7 - Make it shoot far enough to be guaranteed off screen
+        let shootAmount = direction * 1000
+        print("shoot amount \(shootAmount)")
+        
+        // 8 - Add the shoot amount to the current position
+        let realDest = shootAmount + paintball.position
+        print("real dest \(realDest)")
+        
+        
+        // 9 - Create the actions
+        let actionMove = SKAction.moveTo(realDest, duration: 2.0)
+        let actionMoveDone = SKAction.removeFromParent()
+        paintball.runAction(SKAction.sequence([actionMove, actionMoveDone]))
+        
+        
+        
+    }
     
     
     override func touchesEnded(touches: Set<UITouch>, withEvent event: UIEvent?) {
@@ -249,7 +338,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         let touchLocation = touch.locationInNode(self)
         
         
-        roundPlayer.removeAllActions()
+        humanPlayer.removeAllActions()
 
         
         
@@ -257,56 +346,11 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         if !leftButton.containsPoint(touchLocation) && !rightButton.containsPoint(touchLocation) && !upButton.containsPoint(touchLocation) && !downButton.containsPoint(touchLocation){
             
 
+        print("touch location: \(touchLocation)")
         
-        
-        // 2 - Set up initial location of projectile
-        
-        let paintballShape = SKShapeNode(circleOfRadius: 3)
-        paintballShape.fillColor = UIColor.yellowColor()
-        paintballShape.lineWidth = 0
-        
-        let texture = view?.textureFromNode(paintballShape)
-        let paintball = SKSpriteNode(texture: texture)
-        
-        paintball.position = roundPlayer.position
-        
-        paintball.physicsBody = SKPhysicsBody(circleOfRadius: paintball.frame.width/2)
-        //check on this frame later
-        
-        paintball.physicsBody?.dynamic = true
-        paintball.physicsBody?.categoryBitMask = PhysicsCategory.Paintball
-        paintball.physicsBody?.contactTestBitMask = PhysicsCategory.Bunker
-        paintball.physicsBody?.collisionBitMask = PhysicsCategory.None
-        paintball.physicsBody?.usesPreciseCollisionDetection = true
-
-        // 3 - Determine offset of location to projectile
-        let offset = touchLocation - paintball.position
-        
-        // 4 - Bail out if you are shooting down or backwards
-//        if (offset.x < 0) { return }
-        
-        // 5 - OK to add now - you've double checked position
-        addChild(paintball)
-        
-        // 6 - Get the direction of where to shoot
-        let direction = offset.normalized()
-        
-        // 7 - Make it shoot far enough to be guaranteed off screen
-        let shootAmount = direction * 1000
-        
-        // 8 - Add the shoot amount to the current position
-        let realDest = shootAmount + paintball.position
-        
-        // 9 - Create the actions
-        let actionMove = SKAction.moveTo(realDest, duration: 2.0)
-        let actionMoveDone = SKAction.removeFromParent()
-        paintball.runAction(SKAction.sequence([actionMove, actionMoveDone]))
-        
-        
-        
-        
-        
-        
+        shootPaintballFromStartPositionTowards(humanPlayer.position, towardsPosition: touchLocation)
+        shotCounter++
+        shotCountLabel.text = "\(shotCounter)"
         
             
         }
@@ -323,18 +367,32 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         print("did begin contact called")
         
         contact.bodyA
+
+        //bunker has largest bitmask
         
-        // 1
-//        var firstBody: SKPhysicsBody
-//        var secondBody: SKPhysicsBody
-//        if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
-//            firstBody = contact.bodyA
-//            secondBody = contact.bodyB
-//        } else {
-//            firstBody = contact.bodyB
-//            secondBody = contact.bodyA
-//        }
-//        
+        var bunker: SKPhysicsBody
+        var paintball: SKPhysicsBody
+        if contact.bodyA.categoryBitMask < contact.bodyB.categoryBitMask {
+            print("bunker is body B")
+            
+            paintball = contact.bodyA
+            bunker = contact.bodyB
+            
+            paintball.node?.removeFromParent()
+            bunker.node?.removeFromParent()
+            
+            
+        } else {
+            
+            print("bunker is body A")
+
+            paintball = contact.bodyB
+            bunker = contact.bodyA
+            
+            paintball.node?.removeFromParent()
+
+        }
+//
 //        
 //        // 2
 //        if ((firstBody.categoryBitMask & PhysicsCategory.Bunker != 0) &&
